1de0e239abb43498350c669089c7084bbc65204f
AlessioLosi <alessiolosi93@icloud.com>
2024-11-03 00:30:13 +0100

    Initial commit

---
+++ src/main/java/com/w16d5/W16d5Application.java
@@ -0,0 +1,13 @@
+package com.w16d5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W16d5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W16d5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/w16d5/W16d5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w16d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W16d5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
